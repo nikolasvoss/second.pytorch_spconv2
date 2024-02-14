030aaa9aa067149a7c74d466ccf17182ee94c699
niko <nikolas.voss@hm.edu>
2024-02-14 19:05:18 +0100

    changed paths

--- test_nuscenes_train.py
+++ test_nuscenes_train.py
@@ -1,7 +1,7 @@
 from second.pytorch import train
 
 #nuscenes_data_prep('/home/niko/Documents/nuscenes/v1.0-mini', "v1.0-mini", dataset_name="NuscenesDataset", max_sweeps=10)
-train.train(config_path="/home/niko/second.pytorch_spconv2/second/configs/nuscenes/all.pp.mida.config",
+train.train(config_path="/home/niko/second.pytorch_spconv2/second/configs/nuscenes/all.pp.deprecated.config",
     model_dir="/home/niko/Documents/models",
     resume=True)
 
